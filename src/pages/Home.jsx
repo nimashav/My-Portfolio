1cf18c52fb26ee51725ee4c04b235f213bc9df6e
nimshav <nnimasha43@gmail.com>
2025-08-03 23:17:52 +0530

    partially finalized

---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { ThemeToggle } from '../components/ThemeToggle'
+import { Navbar } from '@/components/Navbar';
+import { HeroSection } from '@/components/HeroSection';
+import { AboutSection } from '@/components/AboutSection';
+import { Background } from '@/components/Background';
+import { SkillsSection } from '@/components/SkillsSection';
+import { ProjectsSection } from '@/components/ProjectsSection';
+import { ContactSection } from '@/components/ContactSection';
+import { Footer } from '@/components/Footer';
+
+export const Home = () => {
+  return (
+    <div className='min-h-screen bg-background text-foreground overflow-hidden'>
+      {/**Theme Toggle */}
+      <ThemeToggle />
+      {/**Background effects */}
+      {/* <Background /> */}
+      {/**Navbar */}
+      <Navbar />
+      {/**Main Content */}
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
+      {/**Footer  */}
+      <Footer />
+    </div>
+  );
+};
+
+
+
+
